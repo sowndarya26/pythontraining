edbb9e26935c5677a30b50c8e2613cff8c5790cd
sowndarya26 <37235075+sowndarya26@users.noreply.github.com>
2018-03-10 15:29:48 +0530

    Add files via upload

---
+++ codekata/Assignments/string.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+ astring = "Hello word!"
+ print len(astring)
+
+
+if __name__ == '__main__':
+    main()
